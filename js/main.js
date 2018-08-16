b04511c9c999f577a06ef8ed88d5252fb60a81cb
William Decker <willdecker@gmail.com>
2018-08-16 14:58:37 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+
+
+$(function () {
+
+  // Find all YouTube videos
+  var $allVideos = $("iframe[src^='https://www.youtube.com']"),
+
+    // The element that is fluid width
+    $fluidEl = $(".video-feature");
+
+  // Figure out and save aspect ratio for each video
+  $allVideos.each(function () {
+
+    $(this)
+      .data('aspectRatio', this.height / this.width)
+
+      // and remove the hard coded width/height
+      .removeAttr('height')
+      .removeAttr('width');
+
+  });
+
+  // When the window is resized
+  // (You'll probably want to debounce this)
+  $(window).resize(function () {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function () {
+
+      var $el = $(this);
+      $el
+        .width(newWidth * .8)
+        .height((newWidth * .8) * $el.data('aspectRatio'));
+
+    });
+
+    // Kick off one resize to fix all videos on page load
+  }).resize();
+
+});
